067e83f6f887c8e849999a55d1e258beec9bfc50
Hector Tudela Morales <mthector1@gmail.com>
2025-02-05 09:12:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ secret_key = '1234'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password1@localhost:3306/mibd'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password1@mysql:3306/mibd'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
